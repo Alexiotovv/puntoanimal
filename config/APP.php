ead4ba489645dca5dc0482215ff7c364d33c3600
Alexiotovv <gpsalexvasquez@gmail.com>
2025-01-17 20:16:28 -0500

    url_changed

--- config/APP.php
+++ config/APP.php
@@ -1,6 +1,6 @@
 <?php
  
-const SERVERURL="https://puntoanimal.aywsolution.com/";
+const SERVERURL="https://puntoanimal.aywsolution.com/puntoanimal";
 
 
 date_default_timezone_set ("America/Lima");
